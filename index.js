90ea76f807a9258af2ddd1552981b420183dcda8
geoffrey woodcock <theatomproject@gmail.com>
2017-10-07 18:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var name = "Joe"
 var height = "74"
-
+var name = "Susan"
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
@@ -11,4 +11,3 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
-
